3815c03f0162134f1c7f37489180385f2ba7172d
haykel jabri <haykel.j@gmail.com>
2025-06-04 03:34:12 +0200

    Update apiDjango.js
    fix front authentification and registration

--- devis-front/src/apiDjango.js
+++ devis-front/src/apiDjango.js
@@ -1,9 +1,8 @@
-// src/api.js
+// src/apiDjango.js
 import axios from 'axios';
 
 const apiDjango = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
-  // tu peux ajouter ici les interceptors, headers, etc.
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api/',
 });
 
 export default apiDjango;
